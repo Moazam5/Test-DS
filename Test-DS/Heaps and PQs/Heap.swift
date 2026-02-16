15fa61d40d6404f4a20690c997172c07da3f0c38
moazam5 <m.moazam29@icloud.com>
2026-02-15 18:06:56 -0800

    Add initial implementation of heap data structure

---
+++ Test-DS/Heaps and PQs/Heap.swift
@@ -0,0 +1,44 @@
+//
+//  Heap.swift
+//  Test-DS
+//
+//  Created by Moazam Abass Mir on 2/15/26.
+//
+
+import Foundation
+import Playgrounds
+
+// Heap is a complete binary tree.
+// Heap exists in two flavors, min heap and max heap.
+// This is a generic implementation
+struct Heap {
+	var elements = [Int]()
+	let priority: (Int, Int) -> Bool
+
+	init(sort: @escaping (Int, Int) -> Bool) {
+		self.priority = sort
+	}
+
+	mutating func insert(_ element: Int) {
+		self.elements.append(element)
+		self.siftUp(from: self.elements.count - 1)
+	}
+
+	mutating func siftUp(from index: Int) {
+		var child = index
+		var parent = (child - 1) / 2
+		while child > 0 && priority(self.elements[child], self.elements[parent]) {
+			self.elements.swapAt(child, parent)
+			child = parent
+			parent = (child - 1) / 2
+		}
+	}
+}
+
+
+#Playground {
+	var heap = Heap(sort: >)
+	heap.insert(3)
+	heap.insert(4)
+	print(heap.elements)
+}
