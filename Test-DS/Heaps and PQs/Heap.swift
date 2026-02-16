8a4d1c45c1e4167b8a96119a51282e501f8f9645
moazam5 <m.moazam29@icloud.com>
2026-02-16 13:22:16 -0800

    Add methods for getting heap info.

--- Test-DS/Heaps and PQs/Heap.swift
+++ Test-DS/Heaps and PQs/Heap.swift
@@ -14,6 +14,18 @@ import Playgrounds
 struct Heap {
 	var elements = [Int]()
 	let priority: (Int, Int) -> Bool
+	
+	var isEmpty: Bool {
+		self.elements.isEmpty
+	}
+
+	var count: Int {
+		self.elements.count
+	}
+
+	func peek() -> Int? {
+		self.elements.first
+	}
 
 	init(sort: @escaping (Int, Int) -> Bool) {
 		self.priority = sort
@@ -21,10 +33,11 @@ struct Heap {
 
 	mutating func insert(_ element: Int) {
 		self.elements.append(element)
-		self.siftUp(from: self.elements.count - 1)
+		self.bubbleUp(from: self.elements.count - 1)
+	}
 	}
 
-	mutating func siftUp(from index: Int) {
+	mutating private func bubbleUp(from index: Int) {
 		var child = index
 		var parent = (child - 1) / 2
 		while child > 0 && priority(self.elements[child], self.elements[parent]) {
