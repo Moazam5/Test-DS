df5a5fcd5a5eb5a2498c4d298d8d23d2852ab9f2
moazam5 <m.moazam29@icloud.com>
2026-04-14 18:37:23 -0700

    Add new group for important algorithms and add boyer moore algorithm

---
+++ Test-DS/Important Algorithm /BoyerMoore.swift
@@ -0,0 +1,21 @@
+//
+//  BoyerMoore.swift
+//  Test-DS
+//
+//  Created by Moazam Abass Mir on 4/14/26.
+//
+
+import Foundation
+
+struct ImportantAlgorithmSolution {
+	func majorityElement(_ nums: [Int]) -> Int {
+		var candidate = 0
+		var count = 0
+
+		for num in nums {
+			if count == 0 { candidate = num }
+			count += num == candidate ? 1 : -1
+		}
+		return candidate
+	}
+}
