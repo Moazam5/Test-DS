817d21658176ec41e287ac8c876153acedc2cd97
moazam5 <m.moazam29@icloud.com>
2026-04-21 20:39:17 -0700

    Add greedy problem protocol

--- Test-DS/Greedy/PartitionLabel.swift
+++ Test-DS/Greedy/PartitionLabel.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+import Playgrounds
+
+protocol Greedy {
+	func partitionLabels(_ s: String) -> [Int]
+}
+
+struct GreedySolution {
+	static let shared = GreedySolution()
+
+	func partitionLabels(_ s: String) -> [Int] {
+		var freq = [Character: (start: Int, end: Int)]()
+
+		for (index, char) in s.enumerated() {
+			if let val = freq[char] {
+				freq[char]?.end = index
+			} else {
+				freq[char] = (index, index)
+			}
+		}
+
+		let intervals = freq.values
+		
+		return []
+		}
+}
+
+#Playground {
+	let s = "xyxxyzbzbbisl"
+	GreedySolution.shared.partitionLabels(s)
+}
