dec125f44b0bb44de1ff24996fc9e31b4d095f46
moazam5 <m.moazam29@icloud.com>
2026-05-30 20:45:35 -0700

    Use shared type `MAX` for inf value instead of creating a new one.

--- Test-DS/Graphs/Shortest Path/ShortestPathBellmanFord.swift
+++ Test-DS/Graphs/Shortest Path/ShortestPathBellmanFord.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import Playgrounds
 
 extension GraphsImpl {
 	/// Given an weighted graph with V vertices numbered from 0 to V-1 and E edges, represented by a 2d array edges[][],
@@ -15,8 +16,7 @@ extension GraphsImpl {
 	/// Additionally, if the graph contains a negative weight cycle, return [-1] to indicate that shortest paths cannot be reliably computed.
 	@discardableResult
 	func bellmanFord(_ vertices: Int, _ edges: [[Int]], _ src: Int) -> [Int] {
-		let INF = Int.max / 4
-		var distances = Array(repeating: INF, count: vertices)
+		var distances = Array(repeating: MAX, count: vertices)
 		distances[src] = 0
 
 		for _ in 0..<vertices - 1 {
@@ -25,7 +25,7 @@ extension GraphsImpl {
 				let v = edge[1]
 				let w = edge[2]
 
-				if distances[u] == INF {
+				if distances[u] == MAX {
 					continue
 				}
 				let uToV = distances[u] + w
@@ -39,7 +39,7 @@ extension GraphsImpl {
 			let u = edge[0]
 			let v = edge[1]
 			let w = edge[2]
-			if distances[u] == INF {
+			if distances[u] == MAX {
 				continue
 			}
 			let uToV = distances[u] + w
@@ -51,8 +51,6 @@ extension GraphsImpl {
 	}
 }
 
-// TODO: Add playground with an example.
-import Playgrounds
 #Playground {
 	let d = GraphsImpl.shared.bellmanFord(5, [[2,1,1],[2,3,1],[3,4,1]], 2)
 	let r = d.max() ?? -1
