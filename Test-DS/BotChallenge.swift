28d33b2f410dbea11a15f928437de24a2d186176
moazam5 <m.moazam29@icloud.com>
2026-05-30 20:46:12 -0700

    Add bot challenge from Apple interview.

--- Test-DS/BotChallenge.swift
+++ Test-DS/BotChallenge.swift
@@ -6,3 +6,42 @@
 //
 
 import Foundation
+
+protocol Bot {
+	var keywords: [BotKeywords] { get set }
+}
+
+struct BotKeywords {
+	let title: String
+	let action: (Int, String) -> String
+}
+
+protocol BotParser {
+	func receive(messages: [String], from bot: Bot) -> [[String]]
+}
+
+struct BotImpl: BotParser {
+	func receive(messages: [String], from bot: Bot) -> [[String]] {
+		var res: [[String]] = []
+		var seenKeywords: [String: [BotKeywords]] = [:]
+
+		for message in messages {
+			let item = message.split(separator: ":", maxSplits: 1)
+			guard item.count == 2 else { continue }
+			let person = String(item[0])
+			let contents = String(item[1])
+			res.append([contents])
+
+			for keyword in bot.keywords {
+				if contents.contains(keyword.title) {
+					seenKeywords[person, default: []].append(keyword)
+					if let allKeywords = seenKeywords[person] {
+						let count = allKeywords.filter({ $0.title == keyword.title }).count
+						res.append([keyword.action(count, person)])
+					}
+				}
+			}
+		}
+		return res
+	}
+}
