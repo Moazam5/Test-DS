ceab7746edf28c9c7651f673f94e64a421b6749c
moazam5 <m.moazam29@icloud.com>
2026-06-04 17:01:39 -0700

    Fix playground

--- Test-DS/Dynamic Programming/LCS/LCSProblems+LongestRepeatingSubsequence.swift
+++ Test-DS/Dynamic Programming/LCS/LCSProblems+LongestRepeatingSubsequence.swift
@@ -10,6 +10,7 @@ import Playgrounds
 
 extension LCSSolution {
 	// TODO: Add tests for this, also need to add printing portion which would require function signature change.
+	/// Given a string, print the longest repeating subsequence such that the two subsequence don’t have same string character at same position, i.e., any i’th character in the two subsequences shouldn’t have the same index in the original string.
 	func longestRepeatingSubsequence(_ s: String) -> Int {
 		let (m, n) = (s.count, s.count)
 		let arr1 = Array(s)
@@ -34,28 +35,5 @@ extension LCSSolution {
 
 
 #Playground {
-	let lcs = LCSSolution()
-
-	func lcsTest(_ text1: String, _ text2: String) -> Int {
-		let (m, n) = (text1.count, text2.count)
-		let arr1 = Array(text1)
-		let arr2 = Array(text2)
-		guard !arr1.isEmpty && !arr2.isEmpty else {
-			return 0
-		}
-		var dp = Array(repeating: Array(repeating: 0, count: n + 1), count: m + 1)
-
-		for i in 1...m {
-			for j in 1...n {
-				if arr1[i - 1] == arr2[j - 1] {
-					dp[i][j] = 1 + dp[i - 1][j - 1]
-				} else {
-					dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
-				}
-			}
-		}
-		return dp[m][n]
-	}
-	lcsTest("caaat", "cat")
-	lcsTest("xxyxy", "xy")
+	let res = LCSSolution.shared.longestRepeatingSubsequence("aabebcdd") // 3
 }
